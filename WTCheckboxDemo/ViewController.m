1b4d22497eb4bae2fc8d19b23a14c0bcf9cb1d2a
Duncan Champney <dchampney@wareto.com>
2013-02-08 16:45:40 -0500

    Reworked the storyboard to specify selected and notSelected images for the WTToggleButton using KeyPath & value in the identity inspector.

--- WTCheckboxDemo/ViewController.m
+++ WTCheckboxDemo/ViewController.m
@@ -19,10 +19,27 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
-//  self.theCheckbox.notSelectedImage = [UIImage imageNamed: @"Checkbox.png"];
-//  self.theCheckbox.selectedImage = [UIImage imageNamed: @"Checkbox-selected.png"];
+  
+/*
+There are 3 ways to set the selected and notSelected icons.
+  
+Method 1: set the .selectedImage and notSelectedImage properties directly through code:
+ 
+  self.theCheckbox.notSelectedImage = [UIImage imageNamed: @"Checkbox.png"];
+  self.theCheckbox.selectedImage = [UIImage imageNamed: @"Checkbox-selected.png"];
+  
+Method 2: set the .selectedImageName and notSelectedImageName through code, 
+and the buttons will load the images from the bundle:
+ 
   self.theCheckbox.notSelectedImageName = @"Checkbox.png";
   self.theCheckbox.selectedImageName =  @"Checkbox-selected.png";
+ 
+Method 3: In Interface Builder, select the "identity inspector" for the WTToggleButton object
+ Create keyPath & value entries for selectedImageName and notSelectedImageName, with a string value
+ that specifies the images to load. This lets you specify image names in IB instead of in code.
+ See the identity inspector for the WTToggleButton in this sample project for an example of this.
+ 
+*/
   
   self.theCheckbox.selected = NO;
   [self setupCheckBoxStatusMessage];
